b268ee0e0b1cc576d2565d20e0017f514bc24354
Kevin Edwards <31578240+kedwards0107@users.noreply.github.com>
2024-03-15 08:37:15 -0400

    Update 2020-10-20HalloweenLights.py
    refactored code

--- 2020-10-20HalloweenLights.py
+++ 2020-10-20HalloweenLights.py
@@ -1,281 +1,81 @@
- #!/usr/bin/python
 import RPi.GPIO as GPIO
 import time
 from multiprocessing import Process
 
+# Set GPIO mode
 GPIO.setmode(GPIO.BCM)
 
-
+# Define sleep times
 SleepTimeS = 0.2
 SleepTimeL = 0.5
 
-# init list with pin numbers
-
+# Define control pins for two sets of GPIOs
 ControlPin = [20, 21, 17, 27]
-
 ControlPin2 = [18, 23, 24, 25]
-# loop through pins and set mode and output to 0
 
-for pin in ControlPin:
-    GPIO.setup(pin,GPIO.OUT)
-    GPIO.output(pin,0)
+# Function to set up pins
+def setup_pins(pins):
+    for pin in pins:
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, 0)
 
+# Set up all control pins
+setup_pins(ControlPin)
+setup_pins(ControlPin2)
 
+# Define stepper motor sequences
+seq1 = [[1, 0, 0, 0], [1, 1, 0, 0], [0, 1, 0, 0], [0, 1, 1, 0],
+        [0, 0, 1, 0], [0, 0, 1, 1], [0, 0, 0, 1], [1, 0, 0, 1]]
+seq2 = [[1, 0, 0, 1], [0, 0, 0, 1], [0, 0, 1, 1], [0, 0, 1, 0],
+        [0, 1, 1, 0], [0, 1, 0, 0], [1, 1, 0, 0], [1, 0, 0, 0]]
 
-for pin in ControlPin2:
-    GPIO.setup(pin,GPIO.OUT)
-    GPIO.output(pin,0)
+# Function to create a blinking pattern
+def blink_pattern(pin, duration):
+    GPIO.setup(pin, GPIO.OUT)
+    for _ in range(duration):
+        GPIO.output(pin, GPIO.HIGH)
+        time.sleep(SleepTimeL)
+        GPIO.output(pin, GPIO.LOW)
+        time.sleep(SleepTimeL)
 
-
-seq1 = [ [1,0,0,0],
-	[1,1,0,0],
-	[0,1,0,0],
-	[0,1,1,0],
-	[0,0,1,0],
-	[0,0,1,1],
-	[0,0,0,1],
-	[1,0,0,1] ]
-
-
-seq2 = [ [1,0,0,1],
-	[0,0,0,1],
-	[0,0,1,1],
-	[0,0,1,0],
-	[0,1,1,0],
-	[0,1,0,0],
-	[1,1,0,0],
-	[1,0,0,0] ]
-
+# Define functions for each candy
 def kitkat():
-		GPIO.setup(4,GPIO.OUT)
-		GPIO.output(4,GPIO.HIGH)
-		#insertRow = [now_format, "sour patch kids",1,0,0,0]
-		#sheet.insert_row(insertRow, 2)
-		GPIO.output(4, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(4, GPIO.HIGH)
-		time.sleep(SleepTimeL)
+    blink_pattern(4, 10)
 
 def hershey():
-		GPIO.setup(13, GPIO.OUT)
-		GPIO.output(13, GPIO.HIGH)
-		#insertRow = [now_format, "sour patch kids",1,0,0,0]
-		#sheet.insert_row(insertRow, 2)
-		GPIO.output(13, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(13, GPIO.HIGH)
-		time.sleep(SleepTimeL)
+    blink_pattern(13, 10)
 
 def dots():
-		GPIO.setup(12, GPIO.OUT)
-		GPIO.output(12, GPIO.HIGH)
-		#insertRow = [now_format, "sour patch kids",1,0,0,0]
-		#sheet.insert_row(insertRow, 2)
-		GPIO.output(12, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(12, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-
+    blink_pattern(12, 10)
 
 def nerds():
-		GPIO.setup(26, GPIO.OUT)
-		GPIO.output(26, GPIO.HIGH)
-		#insertRow = [now_format, "sour patch kids",1,0,0,0]
-		#sheet.insert_row(insertRow, 2)
-		GPIO.output(26, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.HIGH)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.LOW)
-		time.sleep(SleepTimeL)
-		GPIO.output(26, GPIO.HIGH)
-		time.sleep(SleepTimeL)
+    blink_pattern(26, 10)
+
+# Function to control stepper motor
+def control_stepper(ControlPin, sequence, steps):
+    for i in range(steps):
+        for halfstep in range(8):
+            for pin in range(4):
+                GPIO.output(ControlPin[pin], sequence[halfstep][pin])
+            time.sleep(0.001)
+
+# Main loop
 try:
-        while True:
-
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(22, GPIO.IN)
-		input_state = GPIO.input(22)
-        if input_state == 0:
-			print("Boom boom Bill")
-		elif input_state ==1:
-			print("dots")
-			Process(target=dots).start()
-			for i in range(564):
-				for halfstep in range(8):
-					for pin in range(4):
-						GPIO.output(ControlPin[pin], seq2[halfstep][pin])
-					time.sleep(0.001)
-			for i in range(564):
-				for halfstep in range(8):
-					for pin in range(4):
-						GPIO.output(ControlPin[pin], seq1[halfstep][pin])
-					time.sleep(0.001)
-
-			time.sleep(0.1)
-
-
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(16, GPIO.IN)
-		input_state = GPIO.input(16)
-        if input_state == 0:
-			print("Boom boom Bill")
-		elif input_state ==1:
-			print("hershey")
-			Process(target=hershey).start()
-			for i in range(564):
-				for halfstep in range(8):
-					for pin in range(4):
-						GPIO.output(ControlPin[pin], seq1[halfstep][pin])
-					time.sleep(0.001)
-			for i in range(564):
-				for halfstep in range(8):
-					for pin in range(4):
-						GPIO.output(ControlPin[pin], seq2[halfstep][pin])
-					time.sleep(0.001)
-			time.sleep(0.1)
-
-		GPIO.setmode(GPIO.BCM)
-        GPIO.setup(19, GPIO.IN)
-		input_state = GPIO.input(19)
-        if input_state == 0:
-			print("Boom boom Bill")
-		elif input_state ==1:
-			print("kitkat")
-			Process(target=kitkat).start()
-			for i in range(564):
-				for halfstep in range(8):
-					for pin in range(4):
-						GPIO.output(ControlPin2[pin], seq2[halfstep][pin])
-					time.sleep(0.001)
-			for i in range(564):
-				for halfstep in range(8):
-					for pin in range(4):
-						GPIO.output(ControlPin2[pin], seq1[halfstep][pin])
-					time.sleep(0.001)
-			time.sleep(0.1)
-
-
-		GPIO.setmode(GPIO.BCM)
-		GPIO.setup(6, GPIO.IN)
-		input_state = GPIO.input(6)
-        if input_state == 0:
-			print("Boom boom Bill")
-		elif input_state ==1:
-			print("nerds")
-			Process(target=nerds).start()
-			for i in range(532):
-				for halfstep in range(8):
-					for pin in range(4):
-						GPIO.output(ControlPin2[pin], seq1[halfstep][pin])
-					time.sleep(0.001)
-			for i in range(532):
-				for halfstep in range(8):
-					for pin in range(4):
-						GPIO.output(ControlPin2[pin], seq2[halfstep][pin])
-					time.sleep(0.001)
-			time.sleep(0.1)
+    while True:
+        for input_pin, action, pins, seq, steps in [
+            (22, dots, ControlPin, seq2, 564),
+            (16, hershey, ControlPin, seq1, 564),
+            (19, kitkat, ControlPin2, seq2, 564),
+            (6, nerds, ControlPin2, seq1, 532)
+        ]:
+            GPIO.setup(input_pin, GPIO.IN)
+            input_state = GPIO.input(input_pin)
+            if input_state == 1:
+                print(action.__name__)
+                Process(target=action).start()
+                control_stepper(pins, seq, steps)
+            time.sleep(0.1)
 
 except KeyboardInterrupt:
-	GPIO.cleanup()
- 	print ("Done")
+    GPIO.cleanup()
+    print("Done")
